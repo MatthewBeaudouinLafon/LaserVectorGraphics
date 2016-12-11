704e779c48432e6a819e6d1a42f4a28e64eb1d92
MatthewBeaudouinLafon <matthew.beaudouin-lafon@students.olin.edu>
2016-12-11 17:12:41 -0500

    SVG parsing framework for RasPi

--- RasPi/main.py
+++ RasPi/main.py
@@ -1,4 +1,80 @@
 import serial
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-print "Hello World"
+# rawPoints = parseSVG(testBezier)
+# formatedPoints = formatPoints(rawPoints)
+# ser.write(formatedPoints)
+
+ser.write(bytearray([2, 1, 1, 10, 10]))
+
+def parseSVG(name)
+    """
+    Take the name of an image as a string, and return a discretized list of points
+    """
+    # Parse XML
+    paths, attributes = svg2paths('images/' + name + '.svg')
+
+    # Discretize Parametric Curve
+    discretePaths = [] # List of paths ie. list of list of points
+    for path in paths: # Each path counts all lines together
+        points = []
+
+        for curve in path:
+            points += [(curve.point(t).real, curve.point(t).imag) for t in np.arange(0, 1, 10/curve.length())]
+
+        discretePaths.append(points)
+
+    return discretePaths
+
+def formatPoints(points, printToConsole=False)
+    """
+    Takes a list of points and formats them to be sent to the Arduino (scaled)
+    """
+    # Find min-max for scaling image
+    minX = np.inf
+    minY = np.inf
+
+    maxX = 0
+    maxY = 0
+    for path in discretePaths:
+        minX = min(zip(*path)[0])
+        maxX = max(zip(*path)[0])
+        minY = min(zip(*path)[1])
+        maxY = max(zip(*path)[1])
+
+    # Scale image
+    scaledPaths = []
+    scaleDown = maxX if maxX > maxY else maxY
+    scaleUp = 200
+
+    for path in discretePaths:
+
+        newPath = []
+        for point in path:
+            x, y = point
+            zerod = (x - minX, y - minY)
+            scaled = (int(zerod[0]*scaleUp/scaleDown), int(zerod[1]*scaleUp/(scaleDown))) # y is divided by a factor of two due to the mirror setup
+            newPath.append(scaled)
+
+        scaledPaths.append(newPath)
+
+    if printToConsole:
+        #Print x, y values to console individually for each curve
+        for path in scaledPaths:
+              print '\nNew Curve'
+              print 'x'
+              for point in zip(*path)[0]:
+                  print point
+
+              print 'y'   
+              for point in zip(*path)[1]:
+                  print point
+
+    toArduino = []
+    for path in scaledPaths:
+        toArduino.append(len(path))
+        for point in path:
+            toArduino.append(point[0])
+            toArduino.append(point[1])
+
+    return toArduino
